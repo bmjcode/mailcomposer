6cbd75d6a47fa27e3291bc9e08e3d64d27437192
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2018-09-03 12:05:34 -0400

    Expand the package docstring

--- mailcomposer/__init__.py
+++ mailcomposer/__init__.py
@@ -4,6 +4,28 @@
 
 mailcomposer aims to provide a simple, cross-platform interface for
 composing emails through an external application like Microsoft Outlook.
+
+mailcomposer provides several classes to support a variety of email
+applications. Available classes are:
+
+  MailComposer
+    The default class, recommended for most applications. This is a
+    special name that's automatically assigned to the most capable of
+    the below classes available on your system.
+
+  OutlookComposer
+    MAPI-based interface for Microsoft Outlook. Requires the pywin32
+    package. This interface is only available on Microsoft Windows.
+
+  DummyMailComposer
+    A dummy class provided for testing, and as a fallback in case no
+    other interfaces are available. This class does not actually call an
+    email application, but will display the message contents on stdout.
+
+mailcomposer also provides its own exception class, MailComposerError,
+for problems that occur during processing. It can, and probably will,
+also raise any of the standard Python exceptions if you attempt something
+you really shouldn't.
 """
 
 from .dummy import DummyMailComposer
