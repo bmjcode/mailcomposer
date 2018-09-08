40008d3212578057abb2be0b4b8da17f0af0b5b8
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2018-09-08 09:24:28 -0400

    Add the "--no-prompt" option to the module demo

--- mailcomposer/__main__.py
+++ mailcomposer/__main__.py
@@ -12,8 +12,8 @@ from . import *
 DESCRIPTION = """\
 Compose an email message through your desktop client.
 
-If at least one recipient is specified, mailcomposer will behave
-like the Unix mail(1) utility:
+If at least one recipient is specified, mailcomposer will default to
+behaving like the Unix mail(1) utility:
 
   * If no subject line is specified using the "-s" argument, it will
     prompt for one.
@@ -21,9 +21,9 @@ like the Unix mail(1) utility:
   * It will then treat all further input as the message body, until
     it encounters either a line containing a single "." or end-of-file.
 
-If no recipient is specified, mailcomposer will open a new message
-immediately without prompting for a subject or body, but will still
-process other command-line arguments.
+If no recipient is specified, or the "--no-prompt" argument is present,
+mailcomposer will open a new message immediately without prompting for
+a subject or body, but will still process other command-line arguments.
 """
 
 FORMATTER_CLASS = argparse.RawTextHelpFormatter
@@ -61,6 +61,9 @@ def demo():
                           help="specify the subject of the message")
 
     mc_args = parser.add_argument_group("mailcomposer-specific arguments")
+    mc_args.add_argument("--no-prompt",
+                         action="store_false", dest="prompt",
+                         help="don't prompt for a message body")
     mc_args.add_argument("--body-format",
                          type=str, action="store",
                          dest="body_format", metavar="format",
@@ -91,7 +94,7 @@ def demo():
                 mc.attach_file(path)
 
     # Behave like mail(1) if a recipient was specified
-    if args.to_addr:
+    if args.prompt and args.to_addr:
         # Prompt for a subject if none was specified
         if not args.subject:
             mc.subject = getline("Subject: ")
